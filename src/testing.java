6a07efb8a41e5a31249e385d27fd7efee143975b
viswayoga <72929164+viswayoga@users.noreply.github.com>
2021-01-24 05:47:33 -0800

    my unkown commit

--- src/testing.java
+++ src/testing.java
@@ -8,9 +8,9 @@ public class testing {
 	public static void main(String[] args) throws SQLException 
 	{
 		
-		Connection cons = DriverManager.getConnection("com.mysql.jdbc.Driver");
+		Connection cons = DriverManager.getConnection("jdbc:mysql://localhost:3306/chapter1","root","9347370743");
 		Statement stmm = cons.createStatement();
-		String s ="UPDATE bujjikam SET age=16 WHERE namee='ramya'";
+		String s ="select book_name from books where book_id='102'";
 		stmm.execute(s);
 		cons.close();
 		
